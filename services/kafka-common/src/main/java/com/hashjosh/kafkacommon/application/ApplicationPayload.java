fc0a085b9e02c2307b8358200d1bca7a813bb188
takumis <113819650+tech-takumis@users.noreply.github.com>
2025-09-19 10:56:04 +0800

    backend: fixed deserialization error in my workflow service application contract in the payload field we add no agrs, all agrs, and required agrs constractor since we are using class not record

--- services/kafka-common/src/main/java/com/hashjosh/kafkacommon/application/ApplicationPayload.java
+++ services/kafka-common/src/main/java/com/hashjosh/kafkacommon/application/ApplicationPayload.java
@@ -1,19 +1,24 @@
 package com.hashjosh.kafkacommon.application;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import java.util.UUID;
 
-@Getter
-@Setter
-@Builder
 @Data
+@Builder
+@NoArgsConstructor // Required for Jackson deserialization
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ApplicationPayload {
+
+    @JsonProperty("applicationTypeId")
     UUID applicationTypeId;
+    @JsonProperty("userId")
     UUID userId;
+    @JsonProperty("status")
     String status;
+    @JsonProperty("version")
     Long version;
 }
