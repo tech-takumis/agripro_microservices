df76f385ff77cb12421d824610759e302cc6ff59
takumis <113819650+tech-takumis@users.noreply.github.com>
2025-11-01 11:16:01 +0800

    Add kafka consumer in my python ai service to consume application submitted for pcic that required ai predection and accuracy

--- services/ai_service/main.py
+++ services/ai_service/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from config.database import Base,engine
 from controller import ai_controller
+from service.kafka_consume_service import consume_application_submit_event,start_consumer_in_thread
 
 app = FastAPI()
 
@@ -10,3 +11,4 @@ app.include_router(ai_controller.router)
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
+    start_consumer_in_thread()
--- services/ai_service/models/ai_result.py
+++ services/ai_service/models/ai_result.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 class AIResult(Base):
     __tablename__ = "ai_result"
     id = Column(Integer, primary_key=True, index=True)
+    applicationId = Column(String)
     result = Column(String)
     prediction = Column(String)
     accuracy = Column(String)
@@ -12,15 +13,17 @@ class AIResult(Base):
 class AIRequestDTO(BaseModel):
     id: int | None = None
     result: str
+    applicationId: str
     prediction: str
     accuracy: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class AIResponseDTO(BaseModel):
     id: int
     result: str
+    applicationId: str
     prediction: str
     accuracy: str
 
