cb1adb78aa152227e81969f70f54896d69a04fe7
amazingAlbertJohn <albertjohn.agbo@carsu.edu.ph>
2025-11-06 08:57:53 +0800

    Fix provider controller

--- services/application/src/main/java/com/hashjosh/application/controller/ProviderController.java
+++ services/application/src/main/java/com/hashjosh/application/controller/ProviderController.java
@@ -10,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProviderController {
     private final ProviderService providerService;
+
+
 }
